0e28e7722157591e51152821c71e15c946efff35
Yusra Ahmed <Yusra718@fullsail.edu>
2012-11-20 23:33:45 -0500

    Added Dummy Data

--- js/JSON.js
+++ js/JSON.js
@@ -21,5 +21,132 @@ var json = {
 		"occasion": 	["Occasion: ", "Christmas"],
         "notes": 		["Notes: ", "Merry Christmas"],
         "packs": 		["How many packs? : ", "2"]
+	},
+	"decor4": 	{
+		"dectype": 		["Type: ", "Ceiling"],
+		"colors": 		["Color(s): ", ["Black","Orange"]],
+		"occasion": 	["Occasion: ", "Halloween"],
+		"notes": 		["Notes: ", "Happy Halloween"],
+		"packs": 		["How many packs? : ", "9"]
+	},
+	"decor5": 	{
+		"dectype": 		["Type: ", "Ceiling"],
+		"colors": 		["Color(s): ", ["Orange","Yellow"]],
+		"occasion": 	["Occasion: ", "Hanukkah"],
+		"notes": 		["Notes: ", "Happy Hanukkah"],
+		"packs": 		["How many packs? : ", "4"]
+	},
+	"decor6": 	{
+		"dectype": 		["Type: ", "Tinsel"],
+		"tinselType": 	["Tinsel Type: ", "Separate Colors"],
+		"colors": 		["Color(s): ", ["Red","Orange","Yellow","Red","Blue"]],
+		"occasion": 	["Occasion: ", "Thanksgiving"],
+		"notes": 		["Notes: ", "Happy Thanksgiving"],
+		"packs": 		["How many packs? : ", "7"]
+	},
+	"decor7": 	{
+		"dectype": 		["Type: ", "Balloons"],
+		"balloonType": 	["Balloon Type: ", "Regular"],
+		"colors": 		["Color(s): ", ["Pink","Purple","Red","White"]],
+		"occasion": 	["Occasion: ", "Quincenera"],
+		"notes": 		["Notes: ", "Happy 15th Birthday"],
+		"packs": 		["How many packs? : ", "10"]
+	},
+	"decor8": 	{
+		"dectype": 		["Type: ", "Balloons"],
+		"colors": 		["Color(s): ", ["Blue","Green","Red","Orange"]],
+		"occasion": 	["Occasion: ", "Bar Mitzvah"],
+		"notes": 		["Notes: ", "Happy 13th Birthday"],
+		"packs": 		["How many packs? : ", "6"]
+	},
+	"decor9": 	{
+		"dectype": 		["Type: ", "Balloons"],
+		"balloonType": 	["Balloon Type: ", "Regular"],
+		"colors": 		["Color(s): ", ["Pink","Purple"]],
+		"occasion": 	["Occasion: ", "Sweet 16"],
+		"notes": 		["Notes: ", "Happy Sweet 16"],
+		"packs": 		["How many packs? : ", "5"]
+	},
+	"decor10": 	{
+		"dectype": 		["Type: ", "Tinsel"],
+		"tinselType": 	["Tinsel Type: ", "Multiple Colors"],
+		"colors": 		["Color(s): ", ["Pink","Red","Blue"]],
+		"occasion": 	["Occasion: ", "Engagement"],
+		"notes": 		["Notes: ", "Congratulations"],
+		"packs": 		["How many packs? : ", "10"]
+	},
+	"decor11": 	{
+		"dectype": 		["Type: ", "Lights"],
+		"colors": 		["Color(s): ", ["Pink","Blue","Green"]],
+		"occasion": 	["Occasion: ", "Wedding"],
+		"notes": 		["Notes: ", "Congratulations"],
+		"packs": 		["How many packs? : ", "10"]
+	},
+	"decor12": 	{
+		"dectype": 		["Type: ", "Ceiling"],
+		"colors": 		["Color(s): ", ["Pink","Purple"]],
+		"occasion": 	["Occasion: ", "Baby Arrival"],
+		"notes": 		["Notes: ", "It's a Girl"],
+		"packs": 		["How many packs? : ", "1"]
+	},
+	"decor13": 	{
+		"dectype": 		["Type: ", "Ceiling"],
+		"colors": 		["Color(s): ", ["Blue","Green"]],
+		"occasion": 	["Occasion: ", "Baby Arrival"],
+		"notes": 		["Notes: ", "It's a Boy"],
+		"packs": 		["How many packs? : ", "1"]
+	},
+	"decor14": 	{
+		"dectype": 		["Type: ", "Tinsel"],
+		"tinselType": 	["Tinsel Type: ", "Multiple Colors"],
+		"colors": 		["Color(s): ", ["Green","White"]],
+		"occasion": 	["Occasion: ", "St. Patrick's Day"],
+		"notes": 		["Notes: ", "Happy St. Patrick's Day"],
+		"packs": 		["How many packs? : ", "10"]
+	},
+	"decor15": 	{
+		"dectype": 		["Type: ", "Lights"],
+		"colors": 		["Color(s): ", ["White","Red"]],
+		"occasion": 	["Occasion: ", "Anniversary"],
+		"notes": 		["Notes: ", "Happy Anniversary"],
+		"packs": 		["How many packs? : ","1"]
+	},
+	"decor16": 	{
+		"dectype": 		["Type: ", "Tinsel"],
+		"tinselType": 	["Tinsel Type: ", "Multiple Colors"],
+		"colors": 		["Color(s): ", ["Blue","White"]],
+		"occasion": 	["Occasion: ", "Graduation"],
+		"notes": 		["Notes: ", "Congrats Grads"],
+		"packs": 		["How many packs? : ", "7"]
+	},
+	"decor17": 	{
+		"dectype": 		["Type: ", "Tinsel"],
+		"tinselType": 	["Tinsel Type: ", "Multiple Colors"],
+		"colors": 		["Color(s): ", ["Red","White","Blue"]],
+		"occasion": 	["Occasion: ", "Independance Day"],
+		"notes": 		["Notes: ", "Happy 4th of July"],
+		"packs": 		["How many packs? : ", "10"]
+	},
+	"decor18": 	{
+		"dectype": 		["Type: ", "Balloons"],
+		"balloonType": 	["Balloon Type: ", "Metallic"],
+		"colors": 		["Color(s): ", ["Blue","Green","White"]],
+		"occasion": 	["Occasion: ", "Job Promotion"],
+		"notes": 		["Notes: ", "Congratulations"],
+		"packs": 		["How many packs? : ", "9"]
+	},
+	"decor19": 	{
+		"dectype": 		["Type: ", "Lights"],
+		"colors": 		["Color(s): ", ["White","Green"]],
+		"occasion": 	["Occasion: ", "Baby Shower"],
+		"notes": 		["Notes: ", "Congratulations"],
+		"packs": 		["How many packs? : ", "3"]
+	},
+	"decor20": 	{
+		"dectype": 		["Type: ", "Ceiling"],
+		"colors": 		["Color(s): ", ["Red","Pink","Blue"]],
+		"occasion": 	["Occasion: ", "Welcome Home"],
+		"notes": 		["Notes: ", "Welcome Home, We Missed You"],
+		"packs": 		["How many packs? : ", "5"]
 	}
 }
